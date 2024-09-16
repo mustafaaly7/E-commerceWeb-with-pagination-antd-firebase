b08783e583c925753e654ca7371bc036491678a2
Mustafa Ali <mustafaaaly27@gmail.com>
2024-09-16 19:48:22 +0500

    card added

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,6 +1,28 @@
 function Card(){
 return(
     <>
+    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+  <a className="block relative h-48 rounded overflow-hidden">
+    <img
+      alt="ecommerce"
+      className="object-cover object-center w-full h-full block"
+      src="https://dummyimage.com/420x260"
+    />
+  </a>
+  <div className="mt-4">
+    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+      CATEGORY
+    </h3>
+    <h2 className="text-gray-900 title-font text-lg font-medium">
+      The Catalyzer
+    </h2>
+    <p className="mt-1">$16.00</p>
+  </div>
+</div>
+
+
+
+
     
     </>
 )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,26 @@
+import { Card } from "../components/card"
+import { Button } from "antd";
+
 function Home(){
     return(
         <>
-        <h1>Home</h1>
+        <div className="flex justify-around my-6">
+        <h1 className="text-center font-bold text-3xl ">Trending Products</h1> 
+        <Button className="font-bold"> See All</Button> 
+
+        </div>
+        <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap -m-4">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        </div>
+  </div>
+</section>
+
         </>
     )
 }
