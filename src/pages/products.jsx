0755075fc8916c1ea30f27a88a8e25563adfc384
Mustafa Ali <mustafaaaly27@gmail.com>
2024-09-17 16:15:30 +0500

    products added on product page

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,10 +1,34 @@
 import { Pagination } from "antd";
+import { useEffect, useState } from "react";
 
 
-function Products(){
-    return(
+function Products() {
+    const [products, setProducts] = useState([])
+    const [loader, setLoader] = useState(false)
+    useEffect(() => {
+        setLoader(true)
+        try {
+            fetch('https://dummyjson.com/products?limit=10&skip=10&select=title,price')
+                .then(res => res.json())
+                .then((data) => {
+                    setProducts(data.products)
+
+                    setLoader(false)
+                });
+
+
+        } catch (error) {
+
+            console.log(error.message);
+
+        }
+
+
+
+    }, [])
+    return (
         <>
-       
+
 
 
 
@@ -13,6 +37,6 @@ function Products(){
     )
 }
 
-export{
+export {
     Products
 }
