64c741229db056c9e644fdd321ce3378510eb50f
Mustafa Ali <mustafaaaly27@gmail.com>
2024-09-18 17:09:32 +0500

    debugging attempt

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -3,68 +3,84 @@ import { useEffect, useState } from "react";
 import { Card } from "../components/card"
 
 import { Link } from "react-router-dom";
+import { SearchOutlined } from "@ant-design/icons";
 
 
 function Products() {
     const [products, setProducts] = useState([])
+    const [searched, setSearched] = useState('')
     const [loader, setLoader] = useState(false)
     const [skip, setSkip] = useState(0)
-    const[total,setTotal]=useState(20)
-    const[limit ,setLimit]=useState(16)
+    const [total, setTotal] = useState(0)
+
+
     useEffect(() => {
+        setLoader(true)
         try {
-            setLoader(true)
+
             fetch(`https://dummyjson.com/products?limit=16&skip=${skip}`)
                 .then(res => res.json())
                 .then((data) => {
                     setProducts(data.products)
-
+                    setTotal(data.total)
                     setLoader(false)
+                    // console.log(products);
+                    // console.log(total);
+
+
+
+
                 });
 
-
         } catch (error) {
-
             console.log(error.message);
+
+
 
         }
 
-
-
     }, [skip])
-    // console.log(products);
+
+
+
+
 
     return (
         <>
+
+
             <section className="text-gray-600 body-font">
 
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-wrap  md:mx-auto m-4 ">
-
                         {loader ? (
-                            <h1 className="  mx-auto text-center font-bold text-6xl text-black">Loading.....</h1>
-                        ) : (
+                            <h1 className=" mx-auto text-center font-bold text-6xl text-black ">Loading.....</h1>
 
+                        ) : (
                             products.map((data) => (
                                 <div className="lg:w-1/4 md:w-1/2 p-4 md:mx-auto mx-auto   shadow my-1  " >
-                                    <Card product={data} />
-                                    <Link to={`/product/id/${data.id}`} key={data.id} >
-                                        <Button>See More </Button>
-                                    </Link>
-                                </div>
-                                // console.log(data.thumbnail)
+                                <Card product={data}  />
+                            <Link  to={`/product/id/${data.id}`} key={data.id} >
+                                <Button>See More </Button>
+                            </Link>
+                            </div>
+                            ))
 
-                            )))}
+                        )}
+
+
+
+
+
+
                     </div>
                 </div>
             </section>
             <br />
-            <Pagination onChange={(numSkip) => setSkip((numSkip - 1) * 16)
-            } align="center" defaultCurrent={1}  pageSize={50}
-            total={total}  />;
-
-            <br /><br />
-
+            <br />
+    <Pagination onChange={(num)=>setSkip((num-1)*16)} align="center" defaultCurrent={1} total={total-20} />
+    <br />
+    <br />
 
         </>
     )
