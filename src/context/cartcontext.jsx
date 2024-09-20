09f3b5b78aaa28aab1b099fee3a8c2a2d5a369d7
Mustafa Ali <mustafaaaly27@gmail.com>
2024-09-20 20:53:12 +0500

    cart context functions creating

--- src/context/cartcontext.jsx
+++ src/context/cartcontext.jsx
@@ -1,24 +1,57 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 
 export const CartContext = createContext()
 
-function CartContextProvider({children}){
-const [cartItem, setCartItems]= useState([])
-const [isLoaded,setIsloaded] = useState(true)
+function CartContextProvider({ children }) {
+    const [cartItem, setCartItems] = useState([])
+    const [isLoaded, setIsloaded] = useState(true)
+
+    useEffect(() => {
+        const items = localStorage.getItem("cartItems")
+        if (items) {
+            setCartItems([...JSON.parse(items)])
+        }
+        setIsloaded(false)
+    }, [])
+
+    useEffect(() => {
+        if (!isLoaded) {
+            localStorage.setItem("cartItems", JSON.stringify(cartItem))
+        }
+
+
+    }, [cartItem])
+
+
+function AddtoCart(item){
+const arr = [...cartItem]
+const itemIndex = arr.findIndex((data)=>data.id == item.id)
+if(itemIndex == -1){
+arr.push({...item, quantity : 1})
+// console.log(arr);
+
+}else{
+    arr[itemIndex].quantity ++
+}
+setCartItems([...arr])
+}
 
 
 
-return(
 
 
-    <CartContext.Provider>
-    {children}
-    </CartContext.Provider>
-)
+
+    return (
+
+
+        <CartContext.Provider value={{cartItem ,setCartItems}}>
+            {children}
+        </CartContext.Provider>
+    )
 
 }
-export{
+export {
     CartContextProvider
 }
